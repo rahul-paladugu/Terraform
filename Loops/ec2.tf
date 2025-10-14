8de76f267fa258e9658b49524387b64028797b3f
rahul paladugu <rahulpaladugu96@gmail.com>
2025-10-15 12:24:41 +1300

    T

---
+++ Loops/ec2.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "mysql" {
+  ami           = "ami-09c813fb71547fc4f"
+  vpc_security_group_ids = [aws_security_group.all_traffic.id]
+  instance_type = var.env == "dev" ? "t2.micro" : "t3.micro" # conditions are declared here
+
+  tags = {
+    Name = "mysql"
+    Terraform = "true"
+  }
+}
+
+resource "aws_security_group" "all_traffic" {
+  name        = "allow all traffic"
+  description = "Allow all traffic"
+  tags = {
+    Name = "allow_all_traffic"
+    }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+      }
+    }
