e8939ac720c51144328ba9527b449112fa5de2d9
rahul paladugu <rahulpaladugu96@gmail.com>
2025-10-15 12:29:25 +1300

    C

--- Loops/ec2.tf
+++ Loops/ec2.tf
@@ -1,13 +1,11 @@
 resource "aws_instance" "mysql" {
+  count = "5"
   ami           = "ami-09c813fb71547fc4f"
   vpc_security_group_ids = [aws_security_group.all_traffic.id]
   instance_type = var.env == "dev" ? "t2.micro" : "t3.micro" # conditions are declared here
-
-  tags = {
-    Name = "mysql"
-    Terraform = "true"
+  tags = var.ec2_tags[count.index]
   }
-}
+
 
 resource "aws_security_group" "all_traffic" {
   name        = "allow all traffic"
--- Loops/variables.tf
+++ Loops/variables.tf
@@ -1,3 +1,10 @@
 variable "env" {
     default = "dev"
 }
+
+variable "ec2_tags" {
+  default = {
+    Name = ["mongodb", "redis", "mysql", "rabbitmq", "catalogue"]
+    Terraform = "True"
+  }
+}
