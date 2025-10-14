6b915baa338fcbafea7318de7bad675f1a32b6ca
rahul paladugu <rahulpaladugu96@gmail.com>
2025-10-14 18:09:15 +1300

    T

--- ec2_instance/r53.tf
+++ ec2_instance/r53.tf
@@ -1,7 +1,9 @@
 resource "aws_route53_record" "roboshop" {
-  zone_id = var.r53_zone
-  name    = $"{var.component[count.index]}.{var.r53_record}
+  count = 4
+  zone_id = "${var.zone_id}"
+  name    = "${var.instances[count.index]}.${var.domain_name}" # mongodb.daws86s.fun
   type    = "A"
-  ttl     = 301
+  ttl     = 1
   records = [aws_instance.terraform[count.index].private_ip]
+  allow_overwrite = true
 }
--- ec2_instance/variables.tf
+++ ec2_instance/variables.tf
@@ -1,45 +1,11 @@
-
-variable "ami_roboshop" {
-
-default = "ami-09c813fb71547fc4f"
+variable "instances" {
+    default = [ "mongodb", "redis", "mysql", "rabbitmq" ]
 }
 
-variable "environment" {
-
-    default = prod
+variable "zone_id" {
+    default = "Z0948150OFPSYTNVYZOY"
 }
 
-
-variable "component" {
-    default = ["mongodb", "redis", "mysql", "rabbitmq"]
-}
-
-variable "egres_to_port" {
-    default = 0
-}
-
-variable "egres_from_port" {
-    default = 0
-}
-
-variable "ingres_from_port" {
-    default = 0
-}
-
-variable "ingres_to_port" {
-    default = 0
-}
-
-variable "cidr" {
-    default = [0.0.0.0/0]
-}
-
-variable "r53_zone" {
-
-    default = Z0711084A6IKM873A3LI
-}
-
-variable "r53_record" {
-
-    default = rscloudservices.icu
+variable "domain_name" {
+    default = "daws86s.fun"
 }
