23972c97bb123b5be4ea76a0eec7a17c25fb2896
rahul paladugu <rahulpaladugu96@gmail.com>
2025-10-15 11:18:03 +1300

    T

--- ec2_instance/ec2.tf
+++ ec2_instance/ec2.tf
@@ -1,9 +1,11 @@
-resource "aws_instance" "terraform" {
+resource "aws_instance" "mysql" {
   ami           = "ami-09c813fb71547fc4f"
   vpc_security_group_ids = [aws_security_group.all_traffic.id]
+  instance_type = "t3.micro"
 
   tags = {
     Name = "mysql"
+    Terraform = "true"
   }
 }
 
