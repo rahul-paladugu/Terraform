32e315532795ae929e1d79c48e7238e66cd05113
rahul paladugu <rahulpaladugu96@gmail.com>
2025-11-04 18:06:51 +1300

    T

--- for_each/r53.tf
+++ for_each/r53.tf
@@ -1,9 +1,9 @@
 resource "aws_route53_record" "roboshop-dns"{
-    for_each = var.instances
+    for_each = aws_instance.roboshop-for-each
     zone_id = "Z0711084A6IKM873A3LI"
     name = "${each.key}.rscloudservices.icu"
     type = "A"
     ttl = 1
-    records = [aws_instance.roboshop-for-each.each.value.private_ip]
+    records = [each.value.private_ip]
     allow_overwrite = true
 }
