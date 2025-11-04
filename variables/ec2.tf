2f6142ecb6c8c9308af9475afaae47e44d52baf4
rahul paladugu <rahulpaladugu96@gmail.com>
2025-11-04 15:04:13 +1300

    T

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "roboshop" {
+resource "aws_instance" "roboshop-var" {
     ami                     = var.ami_id
     instance_type           = var.instance_type
     vpc_security_group_ids  = aws_security_group.allow-all-traffic.id
