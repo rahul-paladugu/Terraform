33f036a6ae11a205f1fcf03eb7c496761533be4b
rahul paladugu <rahulpaladugu96@gmail.com>
2025-11-04 16:55:33 +1300

    T

--- conditional expressions/r53.tf
+++ conditional expressions/r53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "roboshop-r53-records" {
     zone_id = var.zone_id
-    name = roboshop.rscloudservices.icu
+    name = "roboshop.rscloudservices.icu"
     type = "A"
     ttl = 1
     records = [aws_instance.roboshop.private_ip]
