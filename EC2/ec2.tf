40e4b0b8e5d712825b3bfc71c503340473b1e7c6
rahul paladugu <rahulpaladugu96@gmail.com>
2025-11-04 14:25:43 +1300

    ec2

---
+++ EC2/ec2.tf
@@ -0,0 +1,27 @@
+resource "aws-instance" "roboshop" {
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+    tags = {
+        Name = "roboshop"
+    }
+  
+}
+
+resource "aws_security_group" "allow-all-traffic" {
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+tags = {
+    Name = "Allow all traffic"
+  }
+}
