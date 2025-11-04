34a4d29a064d62cff16c618664d7c03e5162ba9b
rahul paladugu <rahulpaladugu96@gmail.com>
2025-11-04 16:11:20 +1300

    T

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -3,12 +3,14 @@ resource "aws_instance" "roboshop_var" {
     instance_type           = var.instance_type
     vpc_security_group_ids  = [aws_security_group.allow_all_traffic.id]
     tags = {
-        name = "roboshop-var"
+        name = "application-use1"
+        terraform = true
     }
 }
 
 resource "aws_security_group" "allow_all_traffic" {
     name = "allow_all_traffic"
+    description = "allow-all-traffic"
     egress {
     from_port        = var.egress_from_port
     to_port          = var.egress_to_port
@@ -22,7 +24,7 @@ resource "aws_security_group" "allow_all_traffic" {
     cidr_blocks      = var.cidr
     }
    tags = {
-    name = "Allow_All_Traffic"
+    name = "Allow-All-Traffic"
     terraform = true
    }
 }
--- variables/r53.tf
+++ variables/r53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "roboshob_r53" {
   zone_id = var.roboshop_r53_hosted_zone
-  name    = "roboshop".var.domain_id
+  name    = "roboshop.${var.domain_id}"
   type    = "A"
   ttl     = 1
   records = [aws_instance.roboshop_var.private_ip]
