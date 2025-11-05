0d52bead9878885f9e165e38d651059e0ad6f9ab
rahul paladugu <rahulpaladugu96@gmail.com>
2025-11-05 13:09:30 +1300

    T

---
+++ remote-statefile/ec2.tf
@@ -0,0 +1,29 @@
+resource "aws_instance" "roboshop" {
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+    tags = {
+        Name = "roboshop"
+        Terraform = true
+    }
+  
+}
+
+resource "aws_security_group" "allow-all-traffic" {
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+tags = {
+    Name = "Allow all traffic"
+    Terraform = true
+  }
+}
