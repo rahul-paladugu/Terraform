9dd2e99e3b6bf91b4c73fcc151499cbded1cbb68
rahul paladugu <rahulpaladugu96@gmail.com>
2026-04-10 11:55:12 +1200

    ec2 & sg creation using terraform

--- EC2/ec2.tf
+++ EC2/ec2.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "roboshop" {
+resource "aws_instance" "jumphost" {
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.micro"
     tags = {
-        Name = "roboshop"
+        Name = "jumphost"
         Terraform = true
     }
   
